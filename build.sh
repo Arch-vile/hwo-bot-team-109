e988749c6020c45e303e34d1bf58d3365d89be99
ra1p3 <raipe.spamfilter@gmail.com>
2012-09-23 23:30:50 +0300

    clears bin

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf bin/fi
 JAVAC=`which javac| head -1`
 $JAVAC -target 1.6 -source 1.6 -d bin -sourcepath ./src/ -cp ./lib/gson-2.2.2.jar:./lib/slf4j-api-1.7.0.jar:./lib/jbox2d-library-2.1.2.2.jar:./lib/jbox2d-testbed-2.1.2.2.jar:./lib/jbox2d-serialization-1.0.0.jar:./lib/log4j-1.2.17.jar src/fi/nakoradio/hwo/main/HWOBot.java 
